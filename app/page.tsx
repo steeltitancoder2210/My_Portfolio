546b91cae6f9852d4a3a67e4cee0cdc4ce745946
prakhar <prakharsinghal52@gmail.com>
2024-07-27 10:50:07 +0530

    Project section added

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,17 @@ import Image from "next/image";
 import Hero from "@/components/hero";
 import { FloatingNav } from "@/components/ui/floatingnavbar";
 import Grid from "@/components/Grid";
+import RecentProject from "@/components/RecentProject";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        {/* <FloatingNav navItems={[{}]}/> */}
+        <FloatingNav navItems={navItems}/>
         <Hero />
         <Grid/>
+        <RecentProject/>
       </div>
     </main>
   );
